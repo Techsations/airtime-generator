e4a404b4d8bb2407b28e8e150c8d3dc7ea23d372
Techsations <akandedamilare.t@gmail.com>
2023-03-23 11:32:13 +0100

    I completed the function for the generate airtime button

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 function generateAirtime() {
     let display = document.getElementById('airtimeGenerated');
-    display.innerText = Math.floor(Math.random() * 1000000000000000)
+    let network = document.getElementById("netWork");
+    let price = document.getElementById("amount");
+    if (network.value === 'none' && price.value === 'empty') {
+        alert('Plaese select a network provider and airtime amount')
+    } 
+    else if 
+     (network.value === 'none') {
+            alert('Plaese select a network provider')
+    } 
+    else if (price.value === 'empty') {
+        alert('Plaese select an Airtime amount')
+} 
+    else {
+        display.innerText = Math.floor(Math.random() * 1000000000000000)
+    }  
 }
