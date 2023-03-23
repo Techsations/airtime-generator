9528da11d63c92a93b4dca13dbad41c7c0bcdf0e
Techsations <akandedamilare.t@gmail.com>
2023-03-23 14:10:32 +0100

    I included basic functions of airtime recharge in my script file

--- script.js
+++ script.js
@@ -1,18 +1,55 @@
+let display = document.getElementById('airtimeGenerated');
+let network = document.getElementById("netWork");
+let price = document.getElementById("amount");
+let recharge = document.getElementById("recharge")
+let randomNumber = Math.floor(Math.random() * 1000000000000000);
+
+
 function generateAirtime() {
-    let display = document.getElementById('airtimeGenerated');
-    let network = document.getElementById("netWork");
-    let price = document.getElementById("amount");
     if (network.value === 'none' && price.value === 'empty') {
-        alert('Plaese select a network provider and airtime amount')
+        alert('Plaese select a network provider and airtime amount');
     } 
     else if 
      (network.value === 'none') {
-            alert('Plaese select a network provider')
+            alert('Plaese select a network provider');
     } 
     else if (price.value === 'empty') {
-        alert('Plaese select an Airtime amount')
+        alert('Plaese select an Airtime amount');
 } 
     else {
-        display.innerText = Math.floor(Math.random() * 1000000000000000)
+        display.innerText = randomNumber
     }  
 }
+
+function loadAirtime() {
+    if (network.value === "mtn" && recharge.value.startsWith('*555*') && recharge.value.includes(randomNumber) && recharge.value.endsWith('#')) {
+        alert( "#" + amount.value + " " + network.value.toUpperCase() + " Recharge is successful")
+    }
+    else if (network.value === "mtn" && recharge.value.startsWith('*555*') != '*555*' && recharge.value.includes(randomNumber) && recharge.value.endsWith('#')) {
+        alert( "Invalid recharge code")
+    }
+        else if (network.value === "glo" && recharge.value.startsWith('*123*') && recharge.value.includes(randomNumber) && recharge.value.endsWith('#')) {
+          alert( "#" + amount.value + " " + network.value.toUpperCase() + " Recharge is successful")
+    }
+    else if (network.value === "glo" && recharge.value.startsWith('*123*') != '*123*' && recharge.value.includes(randomNumber) && recharge.value.endsWith('#')) {
+        alert( "Invalid recharge code")
+    }
+    else if (network.value === "airtel" && recharge.value.startsWith('*126*') && recharge.value.includes(randomNumber) && recharge.value.endsWith('#')) {
+        alert( "#" + amount.value + " " + network.value.toUpperCase() + " Recharge is successful")
+    }
+    else if (network.value === "airtel" && recharge.value.startsWith('*126*') != '*126*' && recharge.value.includes(randomNumber) && recharge.value.endsWith('#')) {
+    alert( "Invalid recharge code")
+    }
+    else if (network.value === "9mobile" && recharge.value.startsWith('*222*') && recharge.value.includes(randomNumber) && recharge.value.endsWith('#')) {
+    alert( "#" + amount.value + " " + network.value.toUpperCase() + " Recharge is successful")
+    }
+    else if (network.value === "9mobile" && recharge.value.startsWith('*222*') != '*222*' && recharge.value.includes(randomNumber) && recharge.value.endsWith('#')) {
+        alert( "Invalid recharge code")
+    }
+     else {
+        alert("Recharge failed")
+    }
+}
+// function loadAirtime(params) {
+    
+// }
